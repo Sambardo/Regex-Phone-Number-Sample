64aa4d3ea5851741c79c145c2899e29296be0685
Kory T <korythacher@gmail.com>
2018-06-19 09:24:32 -0700

    initial commit

---
+++ PhoneNumberMatch.ps1
@@ -0,0 +1,15 @@
+﻿#grab our data
+$file = get-content "$PSScriptRoot\MOCK_DATA.txt"
+
+#make our pattern
+$regex = "\d{3}-\d{3}-\d{4}"  
+
+#loop through each lin
+foreach ($line in $file)
+{
+    #if our line contains our pattern, write the matched data to the screen
+    if($line -match $regex)
+    {
+        $matches[0]
+    }
+}
